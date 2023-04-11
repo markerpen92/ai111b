3df84076942266c44de62cbae6ac4180659d1623
markerpen92 <markchou920219@gmial.com>
2023-04-11 22:16:59 +0800

    V1.2

--- SemiSupervised/FormalVersion/NaiveBayseTF.py
+++ SemiSupervised/FormalVersion/NaiveBayseTF.py
@@ -31,42 +31,10 @@ entropy_loss = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), LR)
 
 def Ranked(featureValue) : #+-5之間
-    if featureValue>=5.0 : return 1
-    elif featureValue<5.0 and featureValue>=4.0 : return 2
-    elif featureValue<3.9 and featureValue>=3.5 : return 3
-    elif featureValue<3.5 and featureValue>=3.0 : return 4
-    elif featureValue<3.0 and featureValue>=2.5 : return 5
-    elif featureValue<2.5 and featureValue>=2.0 : return 6
-    elif featureValue<2.0 and featureValue>=1.6 : return 7
-    elif featureValue<1.6 and featureValue>=1.2 : return 8
-    elif featureValue<1.2 and featureValue>=1.0 : return 9
-    elif featureValue<1.0 and featureValue>=0.8 : return 10
-    elif featureValue<0.8 and featureValue>=0.7 : return 11
-    elif featureValue<0.7 and featureValue>=0.6 : return 12
-    elif featureValue<0.6 and featureValue>=0.5 : return 13
-    elif featureValue<0.5 and featureValue>=0.4 : return 14
-    elif featureValue<0.4 and featureValue>=0.3 : return 15
-    elif featureValue<0.3 and featureValue>=0.2 : return 16
-    elif featureValue<0.2 and featureValue>=0.1 : return 17
-    elif featureValue<0.1 and featureValue>=0.0 : return 18
-    elif featureValue<0.0 and featureValue>=-0.1 : return 19
-    elif featureValue<-0.1 and featureValue>=-0.2 : return 20
-    elif featureValue<-0.2 and featureValue>=-0.3 : return 21
-    elif featureValue<-0.3 and featureValue>=-0.4 : return 22
-    elif featureValue<-0.4 and featureValue>=-0.5 : return 23
-    elif featureValue<-0.5 and featureValue>=-0.6 : return 24
-    elif featureValue<-0.6 and featureValue>=-0.7 : return 25
-    elif featureValue<-0.7 and featureValue>=-0.8 : return 26
-    elif featureValue<-0.8 and featureValue>=-0.9 : return 27
-    elif featureValue<-0.9 and featureValue>=-1.0 : return 28
-    elif featureValue<-1.0 and featureValue>=-1.2 : return 29
-    elif featureValue<-1.2 and featureValue>=-1.6 : return 30
-    elif featureValue<-1.6 and featureValue>=-2.0 : return 31
-    elif featureValue<-2.0 and featureValue>=-2.5 : return 32
-    elif featureValue<-2.5 and featureValue>=-3.0 : return 33
-    elif featureValue<-3.0 and featureValue>=-4.0 : return 34
-    elif featureValue<-4.0 and featureValue>=-5.0 : return 35
-    else : return 36
+    if featureValue>=5.0 : return 500
+    elif featureValue<5.0 and featureValue>-5.0 : 
+        return int(featureValue*100)
+    else : return -500
 
 def ReadLabels(labels , batchSize) : 
     label = []
@@ -86,7 +54,10 @@ def RankedData(out , labels , OutputDataset , batchsize) :
         if outlabel != label[i] : OutputDataset.append([catFeature , dogFeature , "false"])
         else : OutputDataset.append([catFeature , dogFeature , "true"])
 
-def NaiveBayse(out , OutputDataset , batchsize , eps) : 
+def NaiveBayse(out , OutputDataset , labels , batchsize , eps , startbatch , buffersize) : 
+    if buffersize < startbatch : 
+        RankedData(out , labels , OutputDataset , 8)
+        return 
     DataFeature = []
     DataAmount = len(OutputDataset) * batchsize
     for outs in out :
@@ -119,7 +90,8 @@ def NaiveBayse(out , OutputDataset , batchsize , eps) :
             if label == "dog" : out[idx] = torch.tensor([[3.0 , 0.0]] , requires_grad=True)
             else : out[idx] = torch.tensor([[0.0 , 3.0]] , requires_grad=True)
         idx += 1
-   
+    RankedData(out , labels , OutputDataset , 8)
+
 def train():
     model.train()
     OutputDataset = []
@@ -127,10 +99,8 @@ def train():
     for i, data in enumerate(train_loader):
         inputs, labels = data
         out = model(inputs)
-        if idx <= 35 : RankedData(out , labels , OutputDataset , batchsize=8)
-        else : 
-            NaiveBayse(out , OutputDataset , batchsize=8 , eps=0.0003)
-            RankedData(out , labels , OutputDataset , batchsize=8)
+        NaiveBayse(out , OutputDataset , labels , 8 , 0.003 , 35 , idx)
+            
         idx += 1
         loss = entropy_loss(out, labels)
         optimizer.zero_grad()
