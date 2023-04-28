b88f07035f31b2ddc10504da2775b90a8d014d39
markerpen92 <markchou920219@gmial.com>
2023-04-28 13:22:01 +0800

    SUPtesting V2.0

--- SemiSupervised/TestingVersion/SUP_NavieBayes.py
+++ SemiSupervised/TestingVersion/SUP_NavieBayes.py
@@ -12,26 +12,29 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-train_dataset = datasets.ImageFolder("SPATH", transform)
-test_dataset = datasets.ImageFolder("SPATH", transform)
+train_dataset = datasets.ImageFolder("path", transform)
+test_dataset = datasets.ImageFolder("path", transform)
 
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=8, shuffle=True)
 
 model = models.resnet18(pretrained=True)
-                             
+                        
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 2)
-                                         
+
+                                            
 LR = 0.0003
+
 entropy_loss = nn.CrossEntropyLoss()
+
 optimizer = optim.Adam(model.parameters(), LR)
 
-def Ranked(featureValue) : #正負五之間
-    if featureValue>=5.0 : return 500
+def Ranked(featureValue) : #+-5之間
+    if featureValue>=5.0 : return 50000
     elif featureValue<5.0 and featureValue>-5.0 : 
-        return int(featureValue*100)
-    else : return -500
+        return int(featureValue*1000)
+    else : return -50000
 
 def ReadLabels(labels , batchSize) : 
     label = []
@@ -51,61 +54,73 @@ def RankedData(out , labels , OutputDataset , batchsize) :
         if outlabel != label[i] : OutputDataset.append([catFeature , dogFeature , "false"])
         else : OutputDataset.append([catFeature , dogFeature , "true"])
 
-def NaiveBayse(out , OutputDataset , batchsize , eps) : 
-    counts = 0
-    DataFeature = []
-    DataAmount = len(OutputDataset) * batchsize
-    for outs in out :
-        catFeature = Ranked(outs[0].item())
-        dogFeature = Ranked(outs[1].item())
-        DataFeature.append([catFeature , dogFeature])
-    curraset = []
-    falset = []
+# def NaiveBayse(out , OutputDataset , labels , batchsize , eps , startbatch , buffersize) : 
+#     if buffersize < startbatch : 
+#         # RankedData(out , labels , OutputDataset , 8)
+#         return 
+#     DataFeature = []
+#     DataAmount = len(OutputDataset) * batchsize
+#     for outs in out :
+#         catFeature = Ranked(outs[0].item())
+#         dogFeature = Ranked(outs[1].item())
+#         DataFeature.append([catFeature , dogFeature])
+#     curraset = []
+#     falset = []
+#     for data in OutputDataset :
+#         if data[2] == "true" : curraset.append([data[0] , data[1]])
+#         else : falset.append([data[0], data[1]]) 
+#     idx = 0   
+#     for features in DataFeature : 
+#         currasetCatFeatures = falsetCatFeatures = currasetDogFeatures = falsetDogFeatures = 0
+#         for f in falset : 
+#             if features[0] == f[0] : falsetCatFeatures += 1
+#             if features[1] == f[1] : falsetDogFeatures += 1
+#         for c in curraset : 
+#             if features[0] == c[0] : currasetCatFeatures += 1
+#             if features[1] == c[1] : currasetDogFeatures += 1
+#         result = True
+#         label = "cat"
+#         if out[idx][1] > out[idx][0] : label = "dog"
+#         a = (len(falset)/DataAmount)*(falsetCatFeatures/(len(falset)+eps))*(falsetDogFeatures/(len(falset)+eps))
+#         b = (len(curraset)/DataAmount)*(currasetCatFeatures/(len(curraset)+eps))*(currasetDogFeatures/(len(curraset)+eps))
+#         curracy = b/(a+b+eps)
+#         failure = a/(a+b+eps)
+#         if curracy < failure : result = False
+#         if not result : 
+#             if label == "dog" : out[idx] = torch.tensor([[3.0 , 0.0]] , requires_grad=True)
+#             else : out[idx] = torch.tensor([[0.0 , 3.0]] , requires_grad=True)
+#         idx += 1
+#     RankedData(out , labels , OutputDataset , 8)
 
-    for data in OutputDataset : 
-        if data[2] == "true" : curraset.append([data[0] , data[1]])
-        else : falset.append([data[0], data[1]]) 
-    idx = 0   
+def NaiveBayes(outs , labels , OutputDataset , batchsize=8 , mode="train") : 
+    if mode=="train" : 
+        OutputDataset = RankedData(outs , labels , OutputDataset , batchsize)
+        return OutputDataset
+    elif mode=="test" : 
+        answer = []
+        for out in outs : 
+            DogFeature = Ranked(out[0][0].items())
+            CatFeature = Ranked(out[0][1].items())
+            for memorybuffer in OutputDataset : 
+                for memory in memorybuffer : 
+                    if DogFeature == memory[0] and CatFeature == memory[1] and memory[2] == "false" : 
+                        answer.append(torch.tensor([[CatFeature , DogFeature]] , requires_grad=True))     
+                
 
-    for features in DataFeature : 
-        currasetCatFeatures = falsetCatFeatures = currasetDogFeatures = falsetDogFeatures = 0
-        for f in falset : 
-            if features[0] == f[0] : falsetCatFeatures += 1
-            if features[1] == f[1] : falsetDogFeatures += 1
-
-        for c in curraset : 
-            if features[0] == c[0] : currasetCatFeatures += 1
-            if features[1] == c[1] : currasetDogFeatures += 1
-
-        result = True
-        label = "cat"
-        if out[idx][1] > out[idx][0] : label = "dog"
-        a = (len(falset)/DataAmount)*(falsetCatFeatures/(len(falset)+eps))*(falsetDogFeatures/(len(falset)+eps))
-        b = (len(curraset)/DataAmount)*(currasetCatFeatures/(len(curraset)+eps))*(currasetDogFeatures/(len(curraset)+eps))
-        curracy = b/(a+b+eps)
-        failure = a/(a+b+eps)
-        if curracy < failure : 
-            result = False
-            counts += 1
-        if not result : 
-            if label == "dog" : out[idx] = torch.tensor([[1.0 , 0.0]] , requires_grad=True)
-            else : out[idx] = torch.tensor([[0.0 , 1.0]] , requires_grad=True)
-        idx += 1
-
-   
 def train(OutputDataset):
     model.train()
-    
+    OutputDataset = []
     idx = 0
     for i, data in enumerate(train_loader):
         inputs, labels = data
         out = model(inputs)
-        RankedData(out , labels , OutputDataset , batchsize=8)
-        idx += 1
+        # NaiveBayse(out , OutputDataset , labels , 8 , 0.003 , 35 , idx)
         loss = entropy_loss(out, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        if idx == len(train_loader)-2 : OutputDataset = NaiveBayes(out , labels , OutputDataset)
+        idx += 1
 
 def test(OutputDataset):
     model.eval()
@@ -113,37 +128,29 @@ def test(OutputDataset):
     for i, data in enumerate(test_loader):
         inputs, labels = data
         out = model(inputs)
-        NaiveBayse(out , OutputDataset , batchsize=8 , eps=0.0003)
+        NaiveBayes(out , labels , OutputDataset , "test")
         _, predicted = torch.max(out, 1)
         correct += (predicted == labels).sum()
     print(f"Test acc:{(correct.item()/len(test_dataset))*100}%", end="\t\t")
-    t1 = (correct.item()/len(test_dataset))*100
+
     correct = 0
     for i, data in enumerate(train_loader):
         inputs, labels = data
         out = model(inputs)
         _, predicted = torch.max(out, 1)
         correct += (predicted == labels).sum()
-    t2 = (correct.item()/len(train_dataset))*100
     print(f"Train acc:{(correct.item()/len(train_dataset))*100}%")
-    return [t1 , t2]
 
-acc = [0 , 0]
-lastacc = [0 , 0]
-epoch = 0
-while acc[0]/10.0 <= 92.0 : 
+
+for epoch in range(0, 100):
     OutputDataset = []
     print("epoch", epoch)
     train(OutputDataset)
-    ans = test(OutputDataset)
-    acc[0] += ans[0]
-    acc[1] += ans[1]
-    if epoch%10 == 9 : 
-        print("testing improve : " + str((acc[0] - lastacc[0])/10))
-        print("training improve : " + str((acc[1] - lastacc[1])/10))
-        print("testing : " + str(acc[0]/10) + "    training : " + str(acc[1]/10))
-        lastacc = acc
-        acc = [0 , 0]      
-    epoch += 1
+    test(OutputDataset)
+    if (epoch+1)%10 == 0:
+        if aa := input("save? (y/n): ") == "y":
+            torch.save(model.state_dict(), f"./cat_dog{epoch}.pth")
+        if aa == "123": break
+        
 
 torch.save(model.state_dict(), "./cat_dog.pth")
