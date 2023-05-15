d3e509fa25b3fea2fb2bd5ade5c584e14ab820e6
markerpen92 <markchou920219@gmial.com>
2023-05-15 19:43:58 +0800

    Testing V2.7

--- SemiSupervised/TestingVersion/SUP_KNN.py
+++ SemiSupervised/TestingVersion/SUP_KNN.py
@@ -32,9 +32,9 @@ entropy_loss = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), LR)
 
 def ConvertFeature(feature) : 
-    if feature >= 7 : return 7000
-    elif feature < 7 and feature > -7 : return int(feature*1000)
-    elif feature <= -7 : return -7000
+    if feature >= 7 : return 700
+    elif feature < 7 and feature > -7 : return int(feature*100)
+    elif feature <= -7 : return -700
 
 def CreateMemoryBuffer(MemoryBuffer , outputs , labels) : 
     for output , label in zip(outputs , labels) : 
@@ -45,29 +45,61 @@ def CreateMemoryBuffer(MemoryBuffer , outputs , labels) :
         MemoryBuffer.append([DogFeature , CatFeature , OutputIsAccurate])
     return MemoryBuffer
 
-def RBFunction(output , t) : 
+def EuclideanDistance(output , t) : 
+    # if state == "n" : print("n     " , end='\n')
+    # else : print("m     " , end='\n')
     x1 = output[0].item()
     x2 = output[1].item()
+    # print(f"x1 : {x1} , x2 : {x2}")
+    # print(f"t1 : {t[0]} , t2 : {t[1]}")
     TwoNorm = (x1-t[0])**2 + (x2-t[1])**2
-    return np.exp(-0.9*TwoNorm)
+    # print(TwoNorm)
+    return TwoNorm
 
 def KNearistNeighbor(K , MemoryBuffer , outputs) : 
     answers = []
     for output in outputs : 
         neighbor = []
         OutputFeature = torch.tensor([ConvertFeature(output[0].item()) , ConvertFeature(output[1].item())])
-        for i in range(K) : neighbor.append(MemoryBuffer[i])
-        for i in range(len(MemoryBuffer)-K) : 
+        # print(OutputFeature)
+        for i in range(K) :  
+            neighbor.append(MemoryBuffer[i])
+        for i in range(K) : 
             for j in range(K) : 
-                if RBFunction(OutputFeature , neighbor[j]) < RBFunction(OutputFeature , MemoryBuffer[i]) : 
-                    MemoryBuffer[i] = neighbor[j]
+                if EuclideanDistance(OutputFeature , neighbor[i]) < EuclideanDistance(OutputFeature , neighbor[j]) : 
+                    temp = neighbor[i]
+                    neighbor[i] = neighbor[j]
+                    neighbor[j] = temp
+        # input(neighbor)
+        for i in range(K , len(MemoryBuffer)) : 
+            # for j in range(len(neighbor)) : 
+            #     print(MemoryBuffer[i])
+            #     if EuclideanDistance(OutputFeature , neighbor[j] , "n") > EuclideanDistance(OutputFeature , MemoryBuffer[i] , "m") : 
+            #         neighbor[j] = MemoryBuffer[i] 
+            #         break
+            #     input(neighbor)
+            idx = 0
+            IsIn = False
+            # print(MemoryBuffer[i])
+            while EuclideanDistance(OutputFeature , neighbor[idx]) > EuclideanDistance(OutputFeature , MemoryBuffer[i]) : 
+                idx += 1
+                IsIn = True
+                if idx >= len(neighbor) :
+                    idx -= 1
                     break
+            if IsIn : 
+                neighbor[idx] = MemoryBuffer[i]
+
         FalseCount = TrueCount = 0
-        for i in MemoryBuffer : 
+        # print("=============================")
+        # for n in neighbor : 
+        #     print(n)
+        for i in neighbor : 
             if i[2] == False : FalseCount += 1
             else : TrueCount += 1
         if FalseCount > TrueCount : 
             answers.append([output[1].item(), output[0].item()])
+            print("changed")
         else : answers.append([output[0].item(), output[1].item()])
     return torch.tensor(answers , requires_grad=True)
 
@@ -89,15 +121,20 @@ def train(MemoryBuffer , LossBuffer):
         MemoryBuffer = CreateMemoryBuffer(MemoryBuffer , out , labels)
 
 def test(MemoryBuffer):
+    K = 9
     model.eval()
     correct = 0
+    knn_correct = 0
     for i, data in enumerate(test_loader):
         inputs, labels = data
         out = model(inputs)
-        out = KNearistNeighbor(10 , MemoryBuffer , out)
+        knn_out = KNearistNeighbor(K , MemoryBuffer , out)
         _, predicted = torch.max(out, 1)
         correct += (predicted == labels).sum()
+        _, predicted = torch.max(knn_out, 1)
+        knn_correct += (predicted == labels).sum()
     print(f"Test acc:{(correct.item()/len(test_dataset))*100}%", end="\t\t")
+    print(f"KNN Test acc:{(knn_correct.item()/len(test_dataset))*100}%", end="\t\t")
 
     correct = 0
     for i, data in enumerate(train_loader):
@@ -119,9 +156,9 @@ for epoch in range(0, 100):
     end  =datetime.datetime.now()
     diff = end - start
     print(f"======epoch{epoch}======\ntime cost : {diff.microseconds}\n") # 單位微秒
-    print("loss value : ")
-    for lossvalue in LossBuffer : 
-        print(lossvalue)
-    print("==================\n")
+    # print("loss value : ")
+    # for lossvalue in LossBuffer : 
+    #     print(lossvalue)
+    # print("==================\n")
 
 torch.save(model.state_dict(), "./cat_dog100.pth")
